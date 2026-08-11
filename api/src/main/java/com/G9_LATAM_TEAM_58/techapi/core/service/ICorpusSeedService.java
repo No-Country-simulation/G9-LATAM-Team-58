de8e02779bc708690ac4c76bd6fa8399dd4cc954
ebert-escalant <ebertescalant@gmail.com>
2026-08-11 14:36:21 -0500

    refactor: remove unused DTOs and service classes related to corpus seeding

--- api/src/main/java/com/G9_LATAM_TEAM_58/techapi/core/service/ICorpusSeedService.java
+++
@@ -1,8 +0,0 @@
-package com.G9_LATAM_TEAM_58.techapi.core.service;
-
-import com.G9_LATAM_TEAM_58.techapi.common.dto.CorpusSeedRequest;
-import com.G9_LATAM_TEAM_58.techapi.common.dto.CorpusSeedResponse;
-
-public interface ICorpusSeedService {
-    CorpusSeedResponse seed(CorpusSeedRequest request);
-}
